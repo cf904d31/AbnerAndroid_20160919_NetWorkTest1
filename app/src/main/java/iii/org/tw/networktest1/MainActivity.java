2528c9f003982c5c2ddf64793b12ac9fb08fede2
cf904d31 <cf904d31@gmail.com>
2016-09-19 15:20:47 +0800

    NetWorkTest1(沒有包在執行序中直接在主執行序裡無法執行)

--- app/src/main/java/iii/org/tw/networktest1/MainActivity.java
+++ app/src/main/java/iii/org/tw/networktest1/MainActivity.java
@@ -1,13 +1,70 @@
 package iii.org.tw.networktest1;
 
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.net.URL;
+import java.util.Enumeration;
 
 public class MainActivity extends AppCompatActivity {
+    private ConnectivityManager mgr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        mgr = (ConnectivityManager)getSystemService(CONNECTIVITY_SERVICE);
+        NetworkInfo info = mgr.getActiveNetworkInfo();
+        if (info != null && info.isConnected()){
+            try {
+                Enumeration<NetworkInterface> ifs = NetworkInterface.getNetworkInterfaces();
+                while (ifs.hasMoreElements()){
+                    NetworkInterface ip = ifs.nextElement();
+                    Enumeration<InetAddress> ips = ip.getInetAddresses();
+                    while (ips.hasMoreElements()){
+                        InetAddress ia = ips.nextElement();
+                        Log.d("Abner", ia.getHostAddress());
+                    }
+                }
+
+
+            } catch (SocketException e) {
+                e.printStackTrace();
+            }
+        }else{
+            Log.d("Abner", "NOT Connect");
+        }
     }
+
+
+    public void test1(View v) {
+        try {
+            URL url = new URL("http://www.google.com");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.connect();
+            InputStream in = conn.getInputStream();
+            int c; StringBuffer sb = new StringBuffer();
+            while ( (c = in.read()) != -1) {
+                sb.append((char)c);
+            }
+            in.close();
+            Log.d("Abner",sb.toString());
+
+        } catch (Exception e) {
+            Log.d("Abner",e.toString());
+        }
+    }
+
+
 }
